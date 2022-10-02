5d0d17180db5d3cbd2f0ac3c243b692a5b3d4810
CatW-M <wheelermushlin@gmail.com>
2022-10-01 21:45:22 -0600

    set up css, went through tutorial

---
+++ src/components/Square.js
@@ -0,0 +1,10 @@
+const Square = (props) => {
+    return (
+      <button className="square" 
+      onClick={props.handleClick}>
+        {props.value}
+      </button>
+    );
+  }
+
+  export default Square;
