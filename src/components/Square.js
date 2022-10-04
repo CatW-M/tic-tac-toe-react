d35bbdcce96ecb8221d0cbda2d5dc243abe716c7
CatW-M <wheelermushlin@gmail.com>
2022-10-03 18:38:47 -0600

    replaced to do with this.prop.value

--- src/components/Square.js
+++ src/components/Square.js
@@ -4,7 +4,7 @@ class Square extends React.Component {
     render () {
         return (
             <button className="square">
-                {/* To DO */}
+                {this.props.value}
             </button>
         );
     }
