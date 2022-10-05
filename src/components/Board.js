253e300e3966572919a294a4a8d7dbdedf7ddab3
CatW-M <wheelermushlin@gmail.com>
2022-10-04 20:57:09 -0600

    functional component added to board

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,8 +1,43 @@
 import React from "react";
 import Square from "./Square";
 
+//Functional Components
+function Board(props) {
+  const renderSquare = (i) => {
+    return (
+      <div className="square">
+      <Square
+        value={props.squares[i]}
+        onClick={() => props.onClick(i)}
+      />
+       </div>
+    );
+  }
+
+  return (
+    <div>
+        <div className="board-row">
+          {renderSquare(0)}
+          {renderSquare(1)}
+          {renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {renderSquare(3)}
+          {renderSquare(4)}
+          {renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {renderSquare(6)}
+          {renderSquare(7)}
+          {renderSquare(8)}
+        </div>
+      </div >
+  );
+}
+
+
 //Code from tutorial
-class Board extends React.Component {
+/*class Board extends React.Component {
   renderSquare(i) {
     return (
       <div className="square">
@@ -34,7 +69,7 @@ class Board extends React.Component {
       </div>
     );
   }
-}
+}*/
 
 //Code for Tic Tac Toe With Hooks
 // const Board = ({ squares, onClick }) => (
